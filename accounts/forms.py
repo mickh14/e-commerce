d9cd2daaf42e7b279d29a5cc3a3141ab73cd5b08
mickh14 <michael.hurley@expleogroup.com>
2020-02-22 18:58:54 +0000

    add cart

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,42 +3,38 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
-class UserLoginForm(forms.Form):
-    """Forms for loging users in"""
 
-    username = forms.CharField()
+class UserLoginForm(forms.Form):
+    username_or_email = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
 
 
 class UserRegistrationForm(UserCreationForm):
-    """Forms for registering a user"""
-
-    password1 = forms.CharField(
-        label="Password",
-        widget=forms.PasswordInput)
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(
-        label="Password Confirmation", 
-        widget=forms.PasswordInput)
+        label='Password Confirmation',
+        widget=forms.PasswordInput
+    )
 
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
 
-def clean_email(self):
+    def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
         if User.objects.filter(email=email).exclude(username=username):
-            raise forms.ValidationError(u'Email address must be unique')
+            raise forms.ValidationError(u'Email addresses must be unique.')
         return email
 
-def clean_password2(self):
+    def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
 
         if not password1 or not password2:
-            raise ValidationError("Please confirm your password")
-        
+            raise ValidationError("Password must not be empty")
+
         if password1 != password2:
-            raise ValidationError("Passwords must match")
-        
-        return password2        
+            raise ValidationError("Passwords do not match")
+
+        return password2
